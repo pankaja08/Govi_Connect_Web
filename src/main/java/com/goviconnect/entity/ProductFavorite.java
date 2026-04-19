dfed07fd153d6ef94082263e7fba7bc74acc85cd
BawanSharan <sharan@Sharans-MacBook-Air.local>
2026-04-19 20:44:42 +0530

    Added some entity classes: Market, ProductFavorite

---
+++ src/main/java/com/goviconnect/entity/ProductFavorite.java
@@ -0,0 +1,33 @@
+package com.goviconnect.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product_favorite", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "product_id"})
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductFavorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private MarketProduct product;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+}
